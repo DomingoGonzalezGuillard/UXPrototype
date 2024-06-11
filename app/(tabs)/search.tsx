a2e3ab2c76b6d36d88b2bd8ee01b0993b9386bc3
Dommingo Gonzalez <djgonzalez@miuandes.cl>
2024-06-11 12:10:27 -0400

    search bar props added

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 //import { SearchBar } from 'react-native-elements';
@@ -7,12 +7,21 @@ import SearchBar from "../../components/SearchBar";
 
 export default function SearchScreen() {
   const route = useRoute();
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchSubmit = () => {
+    console.log('Search submitted with input:', searchText);
+  };
 //   const buttonName = route.params?.buttonName ?? 'Default';
 
   return (
     <View>
       <Text>Button Name: {"buttonName"}</Text>
-        <SearchBar />
+      <SearchBar
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleSearchSubmit}
+      />
     </View>
   );
 }
