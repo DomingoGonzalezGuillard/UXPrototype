aa5b5cdccbb105a18d3bad5b4a3fa2cf01a441d8
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2024-06-13 21:57:03 -0400

    bigger map

--- app/(tabs)/campus.tsx
+++ app/(tabs)/campus.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
 
 export default function Campus() {
   return (
     <View style={styles.container}>
       <Image
-        source={require('../../assets/images/campus.png')}
-        style={[styles.image, { transform: [{ rotate: '90deg' }] }]}
+        source={require("../../assets/images/campus.png")}
+        style={[styles.image, { transform: [{ rotate: "90deg" }] }]}
       />
     </View>
   );
@@ -15,12 +15,11 @@ export default function Campus() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   image: {
-    width: '100%', // Ocupa todo el ancho de la pantalla
-    height: '100%', // Ocupa todo el alto de la pantalla
-    resizeMode: 'contain', // Escala la imagen para que quepa completamente en el contenedor
+    width: "160%", // Ocupa todo el ancho de la pantalla
+    resizeMode: "contain", // Escala la imagen para que quepa completamente en el contenedor
   },
 });
