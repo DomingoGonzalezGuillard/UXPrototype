d8cbc6f5e63de7d0e09b7a0792bf97fefba068be
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2024-06-13 21:25:24 -0400

    fix index buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,46 @@
-import React, { useState } from 'react';
-import { Image, StyleSheet, View, Button, Text, TouchableOpacity } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  View,
+  Button,
+  Text,
+  TouchableOpacity,
+  Switch,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { Link } from 'expo-router';
-import { BuildingEmoji } from '@/components/BuildingEmoji'; // Asegúrate de importar el componente del emoji
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
+import { BuildingEmoji } from "@/components/BuildingEmoji"; // Asegúrate de importar el componente del emoji
 
 export default function HomeScreen() {
-  const [language, setLanguage] = useState('Español'); // Estado para controlar el idioma
+  // how to set the language options
+  const [language, setLanguage] = useState("Español");
   const [accesibility, setAccesibility] = useState(false);
 
-  const toggleLanguage = (selectedLanguage) => {
+  const toggleLanguage = (selectedLanguage: string) => {
     setLanguage(selectedLanguage); // Cambia el idioma
   };
 
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/portada_universidad.jpg')}
+          source={require("@/assets/images/portada_universidad.jpg")}
           style={styles.universityBackground}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Bienvenido!</ThemedText>
         <HelloWave />
       </ThemedView>
-  
+
       <View style={styles.sectionContainer}>
         <View style={styles.sectionTitleContainer}>
           <Text style={styles.sectionTitle}>Buscar por edificio</Text>
@@ -39,107 +49,136 @@ export default function HomeScreen() {
         <View style={styles.buttonContainer}>
           <View style={styles.buttonRow}>
             <View style={styles.buttonWrapper}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: '#E91E63' }]}>
-
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "#E91E63" }]}
+              >
                 <Link href="/search?query=h" asChild>
-                  <Text style={styles.buttonEmoji}>📜</Text>
+                  <View style={styles.buttonWrapper}>
+                    <Text style={styles.buttonEmoji}>📜</Text>
+                    <Text style={styles.buttonText}>Humanidades</Text>
+                  </View>
                 </Link>
-                  <Text style={styles.buttonText}>Humanidades</Text>
               </TouchableOpacity>
-
             </View>
             <View style={styles.buttonWrapper}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: '#4CAF50' }]}>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "#4CAF50" }]}
+              >
                 <Link href="/search?query=c" asChild>
-                <Text style={styles.buttonEmoji}>🔬</Text>
+                  <View style={styles.buttonWrapper}>
+                    <Text style={styles.buttonEmoji}>🔬</Text>
+                    <Text style={styles.buttonText}>Ciencias</Text>
+                  </View>
                 </Link>
-                <Text style={styles.buttonText}>Ciencias</Text>
-
               </TouchableOpacity>
             </View>
           </View>
           <View style={styles.buttonRow}>
             <View style={styles.buttonWrapper}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: '#FFEB3B' }]}>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "#FFEB3B" }]}
+              >
                 <Link href="/search?query=b" asChild>
-                  <Text style={styles.buttonEmoji}>🏫</Text>
+                  <View style={styles.buttonWrapper}>
+                    <Text style={styles.buttonEmoji}>🏫</Text>
+                    <Text style={styles.buttonText}>Biblioteca</Text>
+                  </View>
                 </Link>
-                <Text style={styles.buttonText}>Biblioteca</Text>
               </TouchableOpacity>
             </View>
             <View style={styles.buttonWrapper}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: '#FF9800' }]}>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "#FF9800" }]}
+              >
                 <Link href="/search?query=i" asChild>
-                  <Text style={styles.buttonEmoji}>🔧</Text>
+                  <View style={styles.buttonWrapper}>
+                    <Text style={styles.buttonEmoji}>🔧</Text>
+                    <Text style={styles.buttonText}>Ingeniería</Text>
+                  </View>
                 </Link>
-                <Text style={styles.buttonText}>Ingeniería</Text>
-
               </TouchableOpacity>
             </View>
           </View>
           <View style={styles.buttonRow}>
             <View style={styles.buttonWrapper}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: '#3F51B5' }]}>
-               <Link href="/search?query=r" asChild>
-                  <Text style={styles.buttonEmoji}>🕚</Text>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "#3F51B5" }]}
+              >
+                <Link href="/search?query=r" asChild>
+                  <View style={styles.buttonWrapper}>
+                    <Text style={styles.buttonEmoji}>🕚</Text>
+                    <Text style={styles.buttonText}>Reloj</Text>
+                  </View>
                 </Link>
-                <Text style={styles.buttonText}>Reloj</Text>
-
               </TouchableOpacity>
             </View>
             <View style={styles.buttonWrapper}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: '#9C27B0' }]}>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "#9C27B0" }]}
+              >
                 <Link href="/search?query=m" asChild>
-
-                 <Text style={styles.buttonEmoji}>✏️</Text>
+                  <View style={styles.buttonWrapper}>
+                    <Text style={styles.buttonEmoji}>🔧</Text>
+                    <Text style={styles.buttonText}>Mecánica</Text>
+                  </View>
                 </Link>
 
                 <Text style={styles.buttonText}>Mecano</Text>
-
               </TouchableOpacity>
             </View>
           </View>
           <View style={styles.buttonRow}>
             <View style={styles.buttonWrapper}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: '#FF5733' }]}>
-              <Link href="/search?query=e" asChild>
-
-                <Text style={styles.buttonEmoji}>🏢</Text>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "#FF5733" }]}
+              >
+                <Link href="/search?query=e" asChild>
+                  <View style={styles.buttonWrapper}>
+                    <Text style={styles.buttonEmoji}>🏢</Text>
+                    <Text style={styles.buttonText}>ESE</Text>
+                  </View>
                 </Link>
-                <Text style={styles.buttonText}>ESE</Text>
-
               </TouchableOpacity>
             </View>
             <View style={styles.buttonWrapper}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: '#795548' }]}>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "#795548" }]}
+              >
                 <Link href="/search?query=ce" asChild>
-                   <Text style={styles.buttonEmoji}>🏛️</Text>
-                 </Link>
-                <Text style={styles.buttonText}>Central</Text>
+                  <View style={styles.buttonWrapper}>
+                    <Text style={styles.buttonEmoji}>🏛️</Text>
+                    <Text style={styles.buttonText}>Central</Text>
+                  </View>
+                </Link>
               </TouchableOpacity>
             </View>
           </View>
         </View>
       </View>
 
-        <View style={styles.sectionContainer}>
-          <TouchableOpacity style={[styles.smallButton, { backgroundColor: '#E7D37F' }]}>
+      <View style={styles.sectionContainer}>
+        <TouchableOpacity
+          style={[styles.smallButton, { backgroundColor: "#E7D37F" }]}
+        >
           <Link href="/campus" asChild>
-          <Text style={styles.buttonEmoji}>🗺️</Text>
+            <View style={styles.buttonWrapper}>
+              -<Text style={styles.buttonEmoji}>🗺️</Text>
+              <Text style={styles.smallButtonText}>Mapa de Campus</Text>
+            </View>
           </Link>
-
-            <Text style={styles.smallButtonText}>Mapa de Campus</Text>
-          
-          </TouchableOpacity>
-        </View>
-
+        </TouchableOpacity>
+      </View>
 
       <View style={styles.languageContainer}>
-        <Image source={require('@/assets/images/idioma.png')} style={styles.languageIcon} />
+        <Image
+          source={require("@/assets/images/idioma.png")}
+          style={styles.languageIcon}
+        />
         <Picker
           selectedValue={language}
           style={styles.languagePicker}
-          onValueChange={(itemValue) => toggleLanguage(itemValue)}>
+          onValueChange={(itemValue) => toggleLanguage(itemValue)}
+        >
           <Picker.Item label="Español" value="Español" />
           <Picker.Item label="English" value="English" />
         </Picker>
@@ -158,64 +197,63 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
     padding: 20,
   },
   universityBackground: {
-    width: '100%',
+    width: "100%",
     height: 300,
-    resizeMode: 'cover',
-    position: 'absolute',
+    resizeMode: "cover",
+    position: "absolute",
     bottom: 0,
     left: 0,
   },
   smallButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 10,
     paddingVertical: 5,
     paddingHorizontal: 10,
     marginTop: 5,
   },
   smallButtonText: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 12,
-    color: '#000000', // White color
+    color: "#000000", // White color
   },
-  
-  
+
   sectionContainer: {
-    backgroundColor: '#f0f0f0',
+    backgroundColor: "#f0f0f0",
     padding: 20,
     borderRadius: 10,
     marginHorizontal: 20,
     marginTop: 20,
   },
   sectionTitleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 10,
   },
   sectionTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginRight: 10,
   },
   buttonContainer: {
     padding: 10,
   },
   buttonRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 10,
   },
   languageContainer: {
     marginTop: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     gap: 10,
   },
   languagePicker: {
@@ -231,30 +269,23 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 5,
     aspectRatio: 1,
-    alignItems: 'center', // Center the text under the button
-  
+    alignItems: "center", // Center the text under the button
   },
   buttonEmoji: {
     fontSize: 35, // Ajusta este valor para cambiar el tamaño del emoji
   },
   buttonText: {
-    textAlign: 'center', // Center the text horizontally
+    textAlign: "center", // Center the text horizontally
     marginTop: 5, // Optional: Add some space between the button and the text
-    fontWeight: 'bold',
-
-  
+    fontWeight: "bold",
   },
-
 
   button: {
     flex: 1,
     marginHorizontal: 5,
     aspectRatio: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 20, // Ajusta este valor para hacer los bordes más redondeados
   },
-
-
-
 });
